d755af162bf5d2882fd02b83d11c9241da4d1df6
Marlon <marlonkenny@gmail.com>
2014-09-12 20:18:05 +0000

    Fixing single page view in progress

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,6 +5,8 @@ class Parser
   POST_URL      = 'https://news.ycombinator.com/item?id='
 
   def self.parse_post(id)
+    url = POST_URL + post.id.to_s
+    page = Nokogiri::HTML(open(url))
     post = Post.new(title, id, url, points, owner)
     post
   end
@@ -33,5 +35,25 @@ class Parser
     @page['items'].map do |item|
       Post.new(item['title'], item['id'], item['url'], item['points'], item['postedBy'])
     end
+  end
+
+  def get_post_id
+    @page.css('form input[name=parent]')[0]['value']    
+  end
+
+  def get_post_title
+    @page.css('.title a')[0].text
+  end
+
+  def get_post_url
+    @page.css('.title a')[0]['href']
+  end
+
+  def get_post_points
+    @page.css('.subtext span')[0].text.match(/\d{0,5}/).to_s
+  end
+
+  def get_post_owner
+    @page.css('.subtext > a:nth-child(2)')[0].text
   end
 end
